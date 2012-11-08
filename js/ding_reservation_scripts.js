0d5048ab6441a32e53a88938cd5bfd4018169a95
AnatolyMuntean <am@inlead.dk>
2012-11-08 11:21:05 +0200

    Quick fix for overlapping ajax commands.

--- js/ding_reservation_scripts.js
+++ js/ding_reservation_scripts.js
@@ -1,13 +1,11 @@
 (function($) {
-  init_behavior = function(ajax, response, status) {
+  enable_reservation = function(ajax, response, status) {
     var entity_id = response.data;
     var forms = $('form');
     var regex = new RegExp(entity_id, 'g');
-
     // Loop through all forms on a page, deeper filtering comes next.
     forms.each(function() {
       form = $(this);
-
       // Wee seek for reservations forms, thus specific form whose item was clicked.
       if (form.attr('id').match(/ding-reservation-reserve-form/g) && form.attr('action').match(regex)) {
         // Dynamic form, so reattach the behaviors.
@@ -20,12 +18,11 @@
         }, 500);
       }
     });
-    
   }
   
   Drupal.behaviors.ding_reservation = {
     attach: function (context, settings) {
-      Drupal.ajax.prototype.commands.init_behavior = init_behavior;
+      Drupal.ajax.prototype.commands.enable_reservation = enable_reservation;
     }
   };
 })(jQuery);

a726dbaba3f34e396f1ba92d7253b28097aa5cb5
Giannis Chionidis <gc@inlead.dk>
2013-06-11 12:57:00 +0200

    case 2828 - Expanding scope of ENTER blocking.

--- js/ding_reservation_ie8.js
+++ js/ding_reservation_ie8.js
@@ -2,14 +2,10 @@
  * Fix for IE8, not allowing form submition/deletion of reservation on ENTER.
  */
 (function($) {
-  $(document).ready(function() {
-    if ($.browser.msie && parseInt($.browser.version, 10) === 8) {
-	    $('input.form-submit').keydown(function(e) {
-        if (e.which == 13) {
-          return false;
-        }
-        return true;
-      });
+  $(document).keydown(function(e) {
+    if (e.keyCode == 13) { 
+        return false;
     }
+    return true;
   });
 })(jQuery);
